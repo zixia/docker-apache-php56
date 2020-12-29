5d2671d522323cf88d07f2b8c2d96b777ee30480
Huan (李卓桓) <zixia@zixia.net>
2020-12-29 15:09:44 +0800

    show version at container start

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,4 +11,5 @@ echo "error_reporting = $PHP_ERROR_REPORTING" >> /etc/php5/cli/php.ini
 
 source /etc/apache2/envvars
 rm -f "$APACHE_PID_FILE"
+cat /webroot/VERSION
 exec /usr/sbin/apache2 -DFOREGROUND "$@"
